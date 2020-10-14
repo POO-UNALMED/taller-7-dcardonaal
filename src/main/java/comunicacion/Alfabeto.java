7500b635cfbf77b02e2d5deccd605838845f6d9b
danielcdona <55594358+danielcdona@users.noreply.github.com>
2020-10-13 22:08:47 -0500

    Update 1

---
+++ src/main/java/comunicacion/Alfabeto.java
@@ -0,0 +1,54 @@
+package comunicacion;
+
+public class Alfabeto extends Pictograma{
+	private static String[] letras;
+	private String interpretacion;
+	
+	
+	
+	public Alfabeto(String origen, String[] letras, String interpretacion) {
+		super(origen);
+		this.letras = letras;
+		this.interpretacion = interpretacion;
+	}
+	
+
+	public int cantidadLetras() {
+		return Alfabeto.letras.length;
+		
+	}
+	
+	public String interpretacion() {
+		return interpretacion;
+		
+	}
+	
+	public String toString() {
+		return "a, b, c, d, e, f, g, h, i, j, k, l, m, n, ñ, o, p, q, r, s, t, u, v, w, x, y, z";
+	}
+
+
+	public static String[] getLetras() {
+		return letras;
+	}
+
+
+	public static void setLetras(String[] letras) {
+		Alfabeto.letras = letras;
+	}
+
+
+	public String getInterpretacion() {
+		return interpretacion;
+	}
+
+
+	public void setInterpretacion(String interpretacion) {
+		this.interpretacion = interpretacion;
+	}
+	
+	
+	
+}
+
+
